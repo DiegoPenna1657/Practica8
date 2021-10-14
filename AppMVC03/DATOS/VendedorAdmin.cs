842e42d66f55a46d60ae1546e876f7b2fc90cee4
DiegoPenna <72095374@ucsm.edu.pe>
2021-10-13 23:19:43 -0500

    Se realizo las 3 experiencias

---
+++ AppMVC03/DATOS/VendedorAdmin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AppMVC03.Models;
+
+namespace AppMVC03.Datos
+{
+    public class VendedorAdmin
+    {
+        public IEnumerable<VENDEDOR> Consultar()
+        {
+            using (VENTASEntities1 contexto = new VENTASEntities1())
+            {
+                return contexto.VENDEDOR.AsNoTracking().ToList();
+            }
+        }
+        public VENDEDOR Consultar(int id)
+        {
+            using (VENTASEntities1 contexto = new VENTASEntities1())
+            {
+                return contexto.VENDEDOR.AsNoTracking().FirstOrDefault(c =>
+               c.CodVendedor == id);
+            }
+        }
+        public void Guardar(VENDEDOR modelo)
+        {
+            using (VENTASEntities1 contexto = new VENTASEntities1())
+            {
+                contexto.VENDEDOR.Add(modelo);
+                contexto.SaveChanges();
+            }
+        }
+        public void Modificar(VENDEDOR modelo)
+        {
+            using (VENTASEntities1 contexto = new VENTASEntities1())
+            {
+                contexto.Entry(modelo).State =
+               System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+        public void Eliminar(VENDEDOR modelo)
+        {
+            using (VENTASEntities1 contexto = new VENTASEntities1())
+            {
+                contexto.Entry(modelo).State =
+               System.Data.Entity.EntityState.Deleted;
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
---
+++ AppMVC03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppMVC03.Startup))]
+namespace AppMVC03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
